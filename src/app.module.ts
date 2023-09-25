f96f6951081d4c462e7bdbce86bfb83899025835
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-09-25 08:33:25 +0500

    generate nest resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,15 @@ import { PrismaService } from './prisma.service'
 import { AuthService } from './auth/auth.service'
 import { JwtService } from '@nestjs/jwt'
 import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
+import { ReviewModule } from './review/review.module';
+import { CategoryModule } from './category/category.module';
+import { OrderModule } from './order/order.module';
+import { StatisticsModule } from './statistics/statistics.module';
+import { PaginationModule } from './pagination/pagination.module';
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, UserModule],
+	imports: [ConfigModule.forRoot(), AuthModule, UserModule, ProductModule, ReviewModule, CategoryModule, OrderModule, StatisticsModule, PaginationModule],
 	controllers: [AppController],
 	providers: [AppService, PrismaService, AuthService, JwtService]
 })
