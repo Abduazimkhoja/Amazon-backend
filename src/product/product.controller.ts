f6c6fb6bde65cb6e01a962ae2cac170d838f1a15
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-09-29 16:40:10 +0500

    product CRUD

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,80 @@
-import { Controller } from '@nestjs/common';
-import { ProductService } from './product.service';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	Param,
+	Post,
+	Put,
+	Query,
+	UsePipes,
+	ValidationPipe
+} from '@nestjs/common'
+import { ProductService } from './product.service'
+import { GetAllProductDto } from './dto/get-all.product.dto'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { ProductDto } from './dto/product.dto'
 
 @Controller('products')
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+	constructor(private readonly productService: ProductService) {}
+
+	// getAll product
+	@UsePipes(new ValidationPipe())
+	@Get()
+	async getAll(@Query() queryDto: GetAllProductDto) {
+		return this.productService.getAll(queryDto)
+	}
+
+	// get product by id
+	@Get(':id')
+	@Auth()
+	async getProduct(@Param('id') id: string) {
+		return this.productService.byId(+id)
+	}
+
+	// get product by id
+	@Get('similar/:id')
+	async getSimilar(@Param('id') id: string) {
+		return this.productService.getSimilar(+id)
+	}
+
+	// get product by slug
+	@Get('by-slug/:slug')
+	async getProductBySlug(@Param('slug') slug: string) {
+		return this.productService.bySlug(slug)
+	}
+
+	// get product by categorySlug
+	@Get('by-category/:categorySlug')
+	async getProductByCategory(@Param('categorySlug') categorySlug: string) {
+		return this.productService.byCategory(categorySlug)
+	}
+
+	// Create product
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Auth()
+	@Post()
+	async createProduct() {
+		return this.productService.create()
+	}
+
+	// Update product
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Put(':id')
+	@Auth()
+	async updateProduct(@Param('id') id: string, @Body() dto: ProductDto) {
+		return this.productService.update(+id, dto)
+	}
+
+	// Update product
+	@HttpCode(200)
+	@Delete(':id')
+	@Auth()
+	async deleteProduct(@Param('id') id: string) {
+		return this.productService.delete(+id)
+	}
 }
