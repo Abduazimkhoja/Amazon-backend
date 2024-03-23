51117c55d05900214a93d67b18526fe4a50d1639
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-03-23 23:49:25 +0500

    refactor: formatting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,37 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { JwtService } from '@nestjs/jwt'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import path from 'path'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
-import { PrismaService } from './prisma.service'
 import { AuthService } from './auth/auth.service'
-import { JwtService } from '@nestjs/jwt'
-import { UserModule } from './user/user.module';
-import { ProductModule } from './product/product.module';
-import { ReviewModule } from './review/review.module';
-import { CategoryModule } from './category/category.module';
-import { OrderModule } from './order/order.module';
-import { StatisticsModule } from './statistics/statistics.module';
-import { PaginationModule } from './pagination/pagination.module';
+import { CategoryModule } from './category/category.module'
+import { OrderModule } from './order/order.module'
+import { PaginationModule } from './pagination/pagination.module'
+import { PrismaService } from './prisma.service'
+import { ProductModule } from './product/product.module'
+import { ReviewModule } from './review/review.module'
+import { StatisticsModule } from './statistics/statistics.module'
+import { UserModule } from './user/user.module'
 
 @Module({
-	imports: [ConfigModule.forRoot(), AuthModule, UserModule, ProductModule, ReviewModule, CategoryModule, OrderModule, StatisticsModule, PaginationModule],
+	imports: [
+		ServeStaticModule.forRoot({
+			rootPath: `${path}/uploads`,
+			serveRoot: '/uploads'
+		}),
+		ConfigModule.forRoot(),
+		AuthModule,
+		UserModule,
+		ProductModule,
+		ReviewModule,
+		CategoryModule,
+		OrderModule,
+		StatisticsModule,
+		PaginationModule
+	],
 	controllers: [AppController],
 	providers: [AppService, PrismaService, AuthService, JwtService]
 })
