e4eccdec02373d77a483ec076143ac27d367f4f1
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-03-25 21:04:23 +0500

    feat: create checkCategoryExists private method and implement error handling

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,12 +1,27 @@
+import { faker } from '@faker-js/faker'
 import { Injectable, NotFoundException } from '@nestjs/common'
 import { PrismaService } from 'src/prisma.service'
 import { returnCategoryObject } from './category-return.object'
 import { CategoryDto } from './category.dto'
-import { faker } from '@faker-js/faker'
+
+type CategoryWhereInput<T, L> =
+	| { id: T; slug?: never }
+	| { id?: never; slug: L }
 
 @Injectable()
 export class CategoryService {
 	constructor(private prisma: PrismaService) {}
+
+	// GET
+	async getAll() {
+		const categories = this.prisma.category.findMany({
+			select: returnCategoryObject
+		})
+
+		if (!categories) throw new NotFoundException('Category not found')
+
+		return categories
+	}
 
 	async byId(id: number) {
 		const category = await this.prisma.category.findUnique({
@@ -14,8 +29,8 @@ export class CategoryService {
 			select: returnCategoryObject
 		})
 
-		if (!category) {throw new NotFoundException('Category not found')}
-		
+		if (!category) throw new NotFoundException('Category not found')
+
 		return category
 	}
 
@@ -30,22 +45,30 @@ export class CategoryService {
 		return category
 	}
 
-	async getAll() {
-		return this.prisma.category.findMany({
-			select: returnCategoryObject
-		})
-	}
-
+	// POST
 	async create(dto: CategoryDto) {
+		const slug = faker.helpers.slugify(dto.name).toLowerCase()
+		await this.checkCategoryExists(
+			{ where: { slug }, select: { slug: true } },
+			'A category with that name already exists',
+			true
+		)
+
 		return this.prisma.category.create({
 			data: {
 				name: dto.name,
-				slug: faker.helpers.slugify(dto.name).toLowerCase()
+				slug
 			}
 		})
 	}
 
+	// PUT
 	async update(id: number, dto: CategoryDto) {
+		await this.checkCategoryExists(
+			{ where: { id }, select: { id: true } },
+			'Category not found'
+		)
+
 		return this.prisma.category.update({
 			where: { id },
 			data: {
@@ -55,7 +78,30 @@ export class CategoryService {
 		})
 	}
 
+	// DELETE
 	async delete(id: number) {
+		await this.checkCategoryExists(
+			{ where: { id }, select: { id: true } },
+			'Category not found'
+		)
+
 		return this.prisma.category.delete({ where: { id } })
+	}
+
+	// 🔏> PRIVATE Functions
+  private async checkCategoryExists(
+		find: {
+			where: CategoryWhereInput<number, string>
+			select: CategoryWhereInput<boolean, boolean>
+		},
+		msg: string,
+		exist: boolean = false // при каком состоянии ответа вызывать ошибку
+	): Promise<void> {
+		// Проверяет есть ли категория или нет.
+		const existingCategory = await this.prisma.category.findUnique(find)
+
+		if (exist ? !!existingCategory : !existingCategory) {
+			throw new NotFoundException(`${msg}`)
+		}
 	}
 }
