86ed0409d58549b818861fe57824b7de0a92c97c
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-06-13 02:55:41 +0500

    refactor: user.dto add ApiPropertyFactory

--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -1,22 +1,28 @@
 import { IsEmail, IsOptional, IsString } from 'class-validator'
+import { ApiPropertyFactory } from 'src/utils/api-property-factory'
 
 export class UserDto {
-	@IsEmail()
-	email: string
-
-	@IsOptional()
-	@IsString()
-	password?: string
-
+	@ApiPropertyFactory('User name', 'Alex')
 	@IsOptional()
 	@IsString()
 	name: string
 
-	@IsOptional()
-	@IsString()
-	avatarPath: string
+	@ApiPropertyFactory('User email address', 'emailName@gmail.com')
+	@IsEmail()
+	email: string
 
+	@ApiPropertyFactory('User phone', '998990004499')
 	@IsOptional()
 	@IsString()
 	phone?: string
+
+	@ApiPropertyFactory('User password', 'f30L@jfsjlapkc')
+	@IsOptional()
+	@IsString()
+	password?: string
+
+	@ApiPropertyFactory('User avatarPath', 'https://image.jpg')
+	@IsOptional()
+	@IsString()
+	avatarPath: string
 }
