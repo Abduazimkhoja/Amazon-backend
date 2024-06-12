cd56a505d2de516e5388e3db64a7e56498930757
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-06-13 02:46:11 +0500

    feat: install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 // import { PrismaService } from './prisma.service'
 
@@ -7,6 +8,14 @@ async function bootstrap() {
 
 	// const prismaService = app.get(PrismaService)
 	// await prismaService.enableShutdownHooks(app)
+
+	const config = new DocumentBuilder()
+		.setTitle('Cats example')
+		.setDescription('The cats API description')
+		.setVersion('1.0')
+		.build()
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('api', app, document)
 
 	app.setGlobalPrefix('api')
 	app.enableCors()
