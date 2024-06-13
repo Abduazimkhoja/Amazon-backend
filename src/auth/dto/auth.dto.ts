23fe3570f650aa997a88c93a7168010e1ce632c8
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-06-13 22:31:44 +0500

    refactor: use auth swagger config object in dto and controller

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,9 +1,12 @@
-import { IsEmail, MinLength, IsString } from 'class-validator'
+import { IsEmail, IsString, MinLength } from 'class-validator'
+import { AuthSwaggerConfig } from '../swagger/auth-swagger.config'
 
 export class AuthDto {
+	@AuthSwaggerConfig.dto.email
 	@IsEmail()
 	email: string
 
+	@AuthSwaggerConfig.dto.password
 	@MinLength(6, {
 		message: 'Password must be at least 6 characters long'
 	})
