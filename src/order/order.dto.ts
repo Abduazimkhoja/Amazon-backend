765a6bf01b2a3792fb69687605b479fef9554ce0
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-06-21 12:00:00 +0500

    refactor: apply Swagger decorators to Orders dto methods

--- src/order/order.dto.ts
+++ src/order/order.dto.ts
@@ -7,26 +7,31 @@ import {
 	IsOptional,
 	ValidateNested
 } from 'class-validator'
+import { OrderSwaggerDtoDecorators } from './swagger/order-swagger.dto'
 
 export class OrderDto {
 	@IsOptional()
 	@IsEnum(EnumOrderStatus)
+	@OrderSwaggerDtoDecorators.status
 	status: EnumOrderStatus
 
 	@IsArray()
 	@ValidateNested({ each: true })
 	@Type(() => OrderItemDto)
+	@OrderSwaggerDtoDecorators.items
 	items: OrderItemDto[]
 }
 
 export class OrderItemDto {
 	@IsNumber()
-  
+  @OrderSwaggerDtoDecorators.quantity
 	quantity: number
 
 	@IsNumber()
+  @OrderSwaggerDtoDecorators.price
 	price: number
 
 	@IsNumber()
+  @OrderSwaggerDtoDecorators.productId
 	productId: number
 }
