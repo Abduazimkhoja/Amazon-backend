cfd63828a483a9bee210cfe30e420271c2111946
Abduazimkhoja <abdulazimkhoja@gmail.com>
2024-06-21 12:00:00 +0500

    refactor: apply Swagger decorators to Orders controller methods

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,11 +1,11 @@
 import {
-	Body,
-	Controller,
-	Get,
-	HttpCode,
-	Post,
-	UsePipes,
-	ValidationPipe
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe
 } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { Auth } from 'src/auth/decorators/auth.decorator'
@@ -13,6 +13,7 @@ import { CurrentUser } from 'src/auth/decorators/user.decorator'
 import { OrderDto } from './order.dto'
 import { OrderService } from './order.service'
 import { PaymentStatusDto } from './payment-status.dto'
+import { OrderSwaggerControllerDecorators } from './swagger/order-swagger.controller'
 
 @ApiTags('Orders')
 @Controller('orders')
@@ -20,12 +21,14 @@ export class OrderController {
 	constructor(private readonly orderService: OrderService) {}
 	@Get()
 	@Auth('admin')
+  @OrderSwaggerControllerDecorators.getAll()
 	getAll() {
 		return this.orderService.getAll()
 	}
 
 	@Get('by-user')
 	@Auth()
+  @OrderSwaggerControllerDecorators.getByUserId()
 	getByUserId(@CurrentUser('id') userId: number) {
 		return this.orderService.getByUserId(userId)
 	}
@@ -34,12 +37,14 @@ export class OrderController {
 	@HttpCode(200)
 	@Post()
 	@Auth()
+  @OrderSwaggerControllerDecorators.placeOrder()
 	placeOrder(@Body() dto: OrderDto, @CurrentUser('id') userId: number) {
 		return this.orderService.placeOrder(dto, userId)
 	}
 
 	@HttpCode(200)
 	@Post('status')
+  @OrderSwaggerControllerDecorators.updateStatus()
 	async updateStatus(@Body() dto: PaymentStatusDto) {
 		return this.orderService.updateStatus(dto)
 	}
